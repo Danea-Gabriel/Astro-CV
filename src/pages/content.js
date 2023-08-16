44953a0da847b6724d11b93c061253abe8fed85f
Danea-Gabriel <gabriell.dannea@gmail.com>
2023-08-16 22:09:26 +0300

    should've gited faster

---
+++ src/pages/content.js
@@ -0,0 +1,170 @@
+// Personal Information -->
+export const name = "Gabriel Danea";
+export const designation = "Frontend Developer";
+export const location = "Cluj, Romania";
+export const pronouns = "he/him";
+
+// About
+export const about =
+  "Hello there, I'm Gabriel. My world revolves around bringing designs to life through frontend development. I find joy in turning concepts into user-friendly experiences that are as visually pleasing as they are functional. ";
+
+// Work Experience -->
+export const work = [
+  {
+    from: "Aug 2022",
+    to: "Aug 2023",
+    role: "E-Media Specialist",
+    org: "MDPI Publishing",
+    org_url: "https://www.mdpi.com",
+    location: "Cluj, Romania",
+    skills: "XML, Web Content Writing, Online Research, Issue Management",
+  },
+  {
+    from: "Oct 2021",
+    to: "May 2022",
+    role: "Researcher",
+    org: "Prisma Analytics GmbH",
+    org_url: "https://www.linkedin.com/company/prisma-analytics/",
+    location: "Cluj, Romania",
+    skills:
+      "Computational linguistics, natural language processing, and machine learning: parsing, word-sense disambiguation, tagging, labeling",
+  },
+  {
+    from: "Feb 2019",
+    to: "Sep 2021",
+    role: "Researcher",
+    org: "Schloer Consulting Group ",
+    org_url: "https://www.linkedin.com/company/prisma-analytics/",
+    location: "Cluj, Romania",
+    skills:
+      "Computational linguistics, natural language processing, and machine learning: parsing, word-sense disambiguation, tagging, labeling",
+  },
+];
+
+// Skills -->
+export const skills = [
+  {
+    title: "TypeScript/JavaScript",
+    description: " ",
+  },
+  {
+    title: "HTML/CSS",
+    description: " ",
+  },
+  {
+    title: "React",
+    description: "",
+  },
+  {
+    title: "NextJS",
+    description: "",
+  },
+  {
+    title: "Astro Js",
+    description: "",
+  },
+  {
+    title: "Learning",
+    description:
+      "I'am trying to learn as much as possible about every framework and library that I can get my hands on.",
+  },
+];
+
+// Projects -->
+export const projects = [
+  {
+    title: "Games For Free",
+    description:
+      "A React-based application developed to provide users with access to a collection of free games. This app enables users to view comprehensive information about each game and allows them to bookmark their favorite games along with direct links for gameplay. ",
+    url: "https://games-for-free-drab.vercel.app/",
+    tech: "React, React Query, Firebase, React Router Dom, Tailwind CSS",
+  },
+  {
+    title: "Crypto Bay",
+    description:
+      "React Project using Coin Gecko API. It let's you see the state of the top 10 cryptocurrencies and also login and have a watchlist of your favorite coins.",
+    url: "https://crypto-bay-bay.vercel.app/ ",
+    tech: "React, Firebase, Tailwind CSS, React Router Dom",
+  },
+];
+
+// Education -->
+export const education = [
+  {
+    from: "2020",
+    to: "2023",
+    institute: "Babeș-Bolyai University ",
+    course: "Bachelor's degree, Computer Science and Economics",
+    location: "Cluj, Romania",
+  },
+  {
+    from: "2023",
+    to: "2023",
+    institute: "Scoala Informala de IT",
+    course: "Web Devlopment",
+    location: "Cluj, Romania",
+  },
+  {
+    from: "2017",
+    to: "2019",
+    institute: "Babeș-Bolyai University ",
+    course:
+      "Master's degree, Security,Intelligence and Competitivity in Organizations",
+    location: "Cluj, Romania",
+  },
+  {
+    from: "2017",
+    to: "2019",
+    institute: "Babeș-Bolyai University ",
+    course: "Master's degree,  Security Management In The Contemporary Society",
+    location: "Cluj, Romania",
+  },
+  {
+    from: "2014",
+    to: "20117",
+    institute: "Babeș-Bolyai University ",
+    course: "Bachelor's degree,  Security Studies",
+    location: "Cluj, Romania",
+  },
+];
+
+// Contact -->
+export const contact = [
+  {
+    source_name: "Email",
+    source: "peter.griffin@example.com",
+  },
+  {
+    source_name: "Github",
+    source: "https://github.com/Danea-Gabriel",
+  },
+  {
+    source_name: "LinkedIn",
+    source: "https://www.linkedin.com/in/gabriel-danea",
+  },
+];
+
+// Certifications in time -->
+
+// Blogs in time maybe -->
+
+// Achievements --> #achievement section
+export const achievements = [
+  {
+    title: "Hard Work Pays Off",
+    year: "2023",
+    decription:
+      "Finished my Bachelor's degree in Computer Science and Economics, finished my Web Development Bootcamp all of that while working full time.",
+  },
+  {
+    title: "Social Circumplex ",
+    year: "2021",
+    decription:
+      "Rethinked the Social Circumplex and created a new model for it.",
+  },
+  {
+    title: "Harworking Student",
+    year: "2019",
+    decription: "Finished two master's degree while working full time",
+  },
+];
